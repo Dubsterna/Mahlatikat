c633b4bc36ad690798e12d2e3521c48dd2cb4823
Teemu Virenius <teemu@Teemus-MBP.Elisa>
2016-11-16 20:26:55 +0200

    Yksinkertainen popover toimimaan omasivulle. Resurssin lisays popup aloitettu.

--- js/omasivu.js
+++ js/omasivu.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
-    console.log("Luukku auki ja risut kyytiin");
-     $(document).on('click', 'button', function(e) {
-            e.stopPropagation();
-            console.log("Funktio toimii");
-        });
-
+    $('[data-toggle="popover"]').popover();
 });
-
