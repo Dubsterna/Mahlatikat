22f2afed4d232630dee19cdc3faa5ff0512bbf64
K2421 <viivi_2@hotmail.com>
2016-11-21 20:16:26 +0200

    Rekisteröinti ja muokkaa käyttäjätietoja
    muutoksia myös hieman rtusivuun

---
+++ etusivu.js
@@ -0,0 +1,10 @@
+//function kirjaudu() {
+	//var nimi = document.getElementById("nimi").value
+    //localStorage.setItem(nimi);
+  //  location.href = "omasivu.html";
+//}
+
+$("#myButton").click(function()  {                    
+  
+    
+});
