4a45571ec1eefe75724a5d983af45f09da63f888
K2421 <viivi_2@hotmail.com>
2016-11-25 12:46:13 +0200

    Add files via upload
    klikkaukset toimmmmiiii

---
+++ muokkaatietoja.js
@@ -0,0 +1,24 @@
+$("#peruuta").click(function()  {                    
+  window.location.href = 'omasivu.html';
+    return false;
+    
+});
+
+
+$("#muokkaa").click(function()  {                    
+  window.location.href = 'omasivu.html';
+    return false;
+    
+});
+
+$("#etusivu").click(function()  {                    
+  window.location.href = 'etusivu.html';
+    return false;
+    
+});
+
+$("#omasivu").click(function()  {                    
+  window.location.href = 'omasivu.html';
+    return false;
+    
+});
